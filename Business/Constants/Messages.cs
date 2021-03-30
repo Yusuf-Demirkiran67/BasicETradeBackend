a09356fc171ade342cc7a1b05d5d933d9ae73672
Yusuf-Demirkiran67 <77542909+Yusuf-Demirkiran67@users.noreply.github.com>
2021-03-30 17:58:39 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductDeleted = "Ürün başarıyla silindi";
+        public static string ProductUpdated = "Ürün başarıyla güncellendi";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string ProductNameAlreadyExists = "Ürün ismi zaten mevcut";
+
+        public static string MaintenanceTimes = "Sistem bakımda";
+        public static string ProductsListed = "Ürün başarıyla listelendi";
+        public static string ProductCountOfCategoryError = "Kategori Ürün Sayısı Hatası";
+        public static string ProductNameAlreadyExits = "Ürün adı daha önce eklendi";
+        public static string CategoryLimitExceded = "Kategori Sınırı Aşıldı";
+    }
+}
